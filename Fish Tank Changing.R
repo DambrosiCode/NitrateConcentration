b91666dc70e07a242ad4ebc29d9a8cf65a014b17
Matthew D'Ambrosio <mattd221@comcast.net>
2020-01-18 10:58:56 -0500

    cleaner

--- Fish Tank Changing.R
+++ Fish Tank Changing.R
@@ -1,51 +1,94 @@
-water_N = 10 #nitrates in tap water
-tank_N = 20  #nitrates in tank
-gal = 8.5    #gallons in tank
-N_inc = 1 #how much do your nitrates increase per day
+library(ggplot2)
 
-#keep these the same#
-change_days = 7       #how often do you change your tank's water?
-perm.change_days = 7  #how often do you change your tank's water?
-###
-
-
-max_days = 280
-days = 0    
-N_change = c(tank_N)
-
-
-water.change.calc = function(total_N, w_change){
-  new_total_N = ((total_N*(w_change)+(water_N*(1-w_change))))
-  return(new_total_N)
+water.change.calc <- function(total_N, water.chng, water.N){
+  #total_N - starting Nitrates in tank
+  #water.chng 0.n amount changed 
+  #returns new nitrate level
+  water.chng <- 1-water.chng
+  
+  new_total_N <- ((total_N*(water.chng)+(water.N*(1-water.chng))))
+  return(new_total_N) #new nitrate concentration 
 }
 
-water.change.graph = function(w_change){
-  while (days <= max_days){
-    tank_N = tank_N + N_inc
-    if (days == change_days) {
-      change_days = change_days + perm.change_days
-      tank_N = water.change.calc(tank_N, w_change)
-      N_change = c(N_change, tank_N)
+water.change.graph <- function(water.chng, max.days = 70, water.N, tank.N, gal, N.inc = 1){
+  #params:
+    #water.chng - what percent of the water do you change 
+    #max.days - how mmany days into the future
+    #water.N - Nitrates in water
+    #tank.N - initial tank Nitrates
+    #gal - gallons of water in tank
+    #N.inc - how much does N increase a day
+  
+  N.chng <- c()
+  
+  cnhg.days <- 7       #how often do you change your tank's water?
+  perm.cnhg.days <- 7  #how often do you change your tank's water?
+  
+  days = 0
+  
+  i <- 0
+  while (days <= max.days){
+    i <- i+1
+    tank.N <- tank.N + N.inc
+    if (days == cnhg.days) {
+      cnhg.days <- cnhg.days + perm.cnhg.days
+      tank.N <- water.change.calc(tank.N, water.chng, water.N)
+      #N.chng <- c(N.chng, tank.N)
+      
       }
-    days = days + 1
+    days <- days + 1
+    N.chng[i] <- (tank.N)
     }
-  return(N_change)
+  return(N.chng[seq(1,length(x),7)])
 }
 
-y_lim = 30
-x_lim = 20
+params <- water.change.graph(water.chng = .4, water.N = 10, tank.N = 20, gal = 8.5)
 
-plot(water.change.graph(1), xlim = c(0,x_lim), ylim = c(water_N,y_lim), xlab = "Week", ylab = "Nitrate ppm", pch = 16, col = 0)
-lines(water.change.graph(1), xlim = c(0,x_lim), ylim = c(water_N,y_lim), xlab = "Week", ylab = "Nitrate ppm", pch = 16, col = 0)
+
+plot(params)
+
+ggplot(data = data.frame(params), aes(y=params, x=c(1:length(params)))) + 
+  geom_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+y_lim <- 30
+x_lim <- 20
+
+
+
+
+
+plot(water.change.graph(1), xlim = c(0,x_lim), 
+     ylim = c(water.N,y_lim), xlab = "Week", ylab = "Nitrate ppm", pch = 16, col = 0)
+
+lines(water.change.graph(1), xlim = c(0,x_lim), 
+      ylim = c(water.N,y_lim), xlab = "Week", ylab = "Nitrate ppm", pch = 16, col = 0)
 
 title("Nitrate Levels per Water Change")
 
-amt = 1 #percent changed
+amt <- 1 #percent changed
+
 while (amt >= 0) {
   points(water.change.graph(amt), col = amt*10+1, pch = 16)
   lines(water.change.graph(amt), col = amt*10+1, pch = 16)
   
-  amt = amt - .1 
+  amt <- amt - .1 
 }
 
 legend(18, 30, legend=c("0%","10%", "20%", "30%", "40%", "50%","60%","70%", "80%","90%","100%" ), col=c(11:1), pch = 16, cex=0.8, title = "Change%")
+
+
+
